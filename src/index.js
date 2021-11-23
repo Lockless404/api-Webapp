aa86370dbb69b0e21d2b2d0b013a459ae75e8879
Jeff Kayombo <jeffkayombo16@gmail.com>
2021-11-23 17:13:57 +0200

    Add comments Popup UI structure, fetch API data & display it

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 import './style.css';
+import CommentsPopUp from './comments';
+const commentsModule = new CommentsPopUp();
+const commentBtns = document.querySelectorAll('.commentBtn');
+console.log(commentBtns);
+if(commentBtns){
+  commentBtns.forEach((btn) => {
+    console.log(btn);
+    btn.addEventListener('click', (e) => {
+      e.preventDefault();
+      const itemId = btn.getAttribute('data');
+      commentsModule.display(itemId);
+    });
+  });
+}
