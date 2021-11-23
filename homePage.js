b111f11eb83cd68eb7c8cecb91e3a89a7ed4cdfb
Molnar Emese <mse212002@gmail.com>
2021-11-23 19:21:58 +0200

    :recycle: refactored the getImages funtion

--- homePage.js
+++ homePage.js
@@ -1,41 +1,50 @@
+const getImages = async () => {
+  const picIds = [1000, 1002, 1003, 1015, 1021, 1022];
+  const pics =[]
+  for(let i = 0; i < picIds.length; i++){
+    const urlToFetch= `https://picsum.photos/id/${picIds[i]}/info`
+    const images = await fetch(urlToFetch)
+    .then((res) => res.json())
+    .then((resData) => resData)
+    .catch((err) => err);
+  pics.push(images)
+  }
+  return pics;
+}
+
 const getPics = async () => {
   const picSection = document.getElementById('item-list');
   const rowOne = document.createElement('div');
   const rowTwo = document.createElement('div');
   rowOne.setAttribute('class', 'f-row');
   rowTwo.setAttribute('class', 'f-row');
-  const picIds = [1000, 1002, 1003, 1015, 1021, 1022];
-  const allPics = [];
-  for(let i = 0; i < picIds.length + 1; i++) {
-    const pic = await fetch(`https://picsum.photos/id/${picIds[i]}/info`);
-    pic.json().then((img) => {
-      allPics.push(img);
-    })
-  };
   let idx = 0;
-  for(const item in allPics) {
-    console.log(allPics)
-    console.log(allPics[item].url)
-    const listItem = document.createElement('div');
-    listItem.innerHTML = `<div class='f-col'>
-      <img src='${allPics[item].download_url}' alt=''>
-      <div class='f-row'>
-        <p>Exhibition ${idx}</p>
-        <p>5 Likes</p>
+  getImages().then((v)=>{
+    v.forEach((img)=>{
+      const listItem = document.createElement('div');
+      listItem.innerHTML = `<div class='f-col'>
+        <img src='${v[idx].download_url}' alt=''>
+        <div class='f-row'>
+          <p>Exhibition ${idx}</p>
+          <p>5 Likes</p>
+        </div>
+        <button type='button' class='comments'>Comments</button>
+        <button type='button' class='reservations'>Reservations</button>
       </div>
-      <button type='button' class='comments'>Comments</button>
-      <button type='button' class='reservations'>Reservations</button>
-    </div>
-    `;
-    if (idx < 3) {
-      rowOne.appendChild(listItem);
-    } else {
-      rowTwo.appendChild(listItem);
-    };
-    idx += 1;
-  }
+      `;
+      if (idx < 3) {
+        rowOne.appendChild(listItem);
+      } else {
+        rowTwo.appendChild(listItem);
+      };
+      idx += 1;
+    })
+  })
   picSection.append(rowOne, rowTwo);
-  return JSON.stringify(allPics);
+};
+
+const postLikes = async (imgId) => {
+  await fetch ()
 }
 
 export default getPics;
